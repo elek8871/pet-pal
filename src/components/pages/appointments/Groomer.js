714dbb6e242a880ab3aa83474c4af4f44b8024dc
Moodybleu <meganbenn27@icloud.com>
2022-10-28 08:17:25 -0700

    makes forms for groomer and playdate

--- src/components/pages/appointments/Groomer.js
+++ src/components/pages/appointments/Groomer.js
@@ -1,8 +1,95 @@
+import { useState } from "react"
+import axios from "axios"
+import { useNavigate, useParams } from "react-router-dom" 
+
+
 export default function Groomer(){
-    
+
+
+    const [form, setForm] = useState({
+        date: "",
+        business_name: "",
+        address: "",
+        phone: "",
+    })
+
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const navigate = useNavigate()
+    const {id} = useParams()
+
+    // submit event handler
+    const handleSubmit = async e => {   
+        e.preventDefault()
+        try {
+            const token = localStorage.getItem('jwt')
+            const options = {
+                headers: {
+                    'Authorization': token
+                }
+            }
+
+            const response = await axios.post(`/pet/${id}/appointment`, form, options)
+            console.log(response.data)
+            console.log(form)
+            navigate('/user/profile')
+        } catch (err) {
+            console.warn(err)
+            if (err.response) {
+                setErrorMessage(err.response.data.message)
+            }
+        }
+    }
     return(
         <div>
-            I am the groomers appointments page 
-        </div>
+        <h2>Let me just pencil you in...</h2>
+        <p>{errorMessage}</p>
+
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor='date'><h2>Date:</h2></label>
+                <input 
+                    type='text'
+                    id='date'
+                    value={form.date}
+                    placeholder="What date was your visit?"
+                    onChange={e => setForm ({ ...form, date: e.target.value})}
+                    />
+            </div>
+            <div>
+                <label htmlFor='business_name'><h2>Business name:</h2></label>
+                <input 
+                    type='text'
+                    id='business_name'
+                    value={form.business_name}
+                    placeholder='Where is you pet getting groomed?'
+                    onChange={e => setForm ({ ...form, business_name: e.target.value})}
+                    />
+            </div>
+            <div>
+                <label htmlFor='address'><h2>Address:</h2></label>
+                <input 
+                    type='text'
+                    id='address'
+                    value={form.address}
+                    placeholder="groomer's address?"
+                    onChange={e => setForm ({ ...form, address: e.target.value})}
+                    />
+            </div>
+            <div>
+                <label htmlFor='phone'><h2>Phone:</h2></label>
+                <input 
+                    type='text'
+                    id='phone'
+                    value={form.phone}
+                    placeholder="Groomer's phone number?"
+                    onChange={e => setForm ({ ...form, phone: e.target.value})}
+                    />
+            </div>
+
+            <button type='submit'><h3>Add your appointment</h3></button>
+
+        </form>
+    </div>
     )
 }
--- src/components/pages/appointments/Play_date.js
+++ src/components/pages/appointments/Play_date.js
@@ -1,8 +1,95 @@
-export default function Play_dates(){
-    
+import { useState } from "react"
+import axios from "axios"
+import { useNavigate, useParams } from "react-router-dom" 
+
+
+export default function Groomer(){
+
+
+    const [form, setForm] = useState({
+        day: "",
+        place: "",
+        date: "",
+        phone: "",
+    })
+
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const navigate = useNavigate()
+    const {id} = useParams()
+
+    // submit event handler
+    const handleSubmit = async e => {   
+        e.preventDefault()
+        try {
+            const token = localStorage.getItem('jwt')
+            const options = {
+                headers: {
+                    'Authorization': token
+                }
+            }
+
+            const response = await axios.post(`/pet/${id}/appointment`, form, options)
+            console.log(response.data)
+            console.log(form)
+            navigate('/user/profile')
+        } catch (err) {
+            console.warn(err)
+            if (err.response) {
+                setErrorMessage(err.response.data.message)
+            }
+        }
+    }
     return(
         <div>
-            I am the Play dates page
-        </div>
+        <h2>Let me just pencil you in</h2>
+        <p>{errorMessage}</p>
+
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor='day'><h2>Day:</h2></label>
+                <input 
+                    type='text'
+                    id='day'
+                    value={form.day}
+                    placeholder="What day is your date?"
+                    onChange={e => setForm ({ ...form, day: e.target.value})}
+                    />
+            </div>
+            <div>
+                <label htmlFor='place'><h2>Location:</h2></label>
+                <input 
+                    type='text'
+                    id='place'
+                    value={form.place}
+                    placeholder='Where is your pets date?'
+                    onChange={e => setForm ({ ...form, place: e.target.value})}
+                    />
+            </div>
+            <div>
+                <label htmlFor='date'><h2>Date:</h2></label>
+                <input 
+                    type='text'
+                    id='date'
+                    value={form.date}
+                    placeholder="Who's your furry friends date?"
+                    onChange={e => setForm ({ ...form, date: e.target.value})}
+                    />
+            </div>
+            <div>
+                <label htmlFor='phone'><h2>Phone:</h2></label>
+                <input 
+                    type='text'
+                    id='phone'
+                    value={form.phone}
+                    placeholder="What is your date's phone number?"
+                    onChange={e => setForm ({ ...form, phone: e.target.value})}
+                    />
+            </div>
+
+            <button type='submit'><h3>Add your play date</h3></button>
+
+        </form>
+    </div>
     )
 }
