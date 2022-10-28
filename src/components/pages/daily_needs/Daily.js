dd8e0bccdb06675038d9f24c35ea2aad5388fba1
Moodybleu <meganbenn27@icloud.com>
2022-10-28 11:00:43 -0700

    attempting date picker calendar

--- src/components/pages/daily_needs/Daily.js
+++ src/components/pages/daily_needs/Daily.js
@@ -2,6 +2,7 @@ import Food from "./Food"
 import Walk from "./Walk"
 import Potty_trip from "./Potty_trip"
 import Meds from "./Meds"
+import TableDatePicker from "./TableDatePicker"
 
 export default function Daily(){
     
@@ -11,7 +12,7 @@ export default function Daily(){
             <Food/>
             <Walk/>
             <Potty_trip/>
-            <Meds/>
+            <TableDatePicker/>
         </div>
     )
 }
