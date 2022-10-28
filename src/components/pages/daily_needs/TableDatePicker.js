70fed766dfa13cde7005256e2b0626762705e804
Moodybleu <meganbenn27@icloud.com>
2022-10-28 11:14:45 -0700

    Adds appointment calendar

--- src/components/pages/daily_needs/TableDatePicker.js
+++ src/components/pages/daily_needs/TableDatePicker.js
@@ -6,8 +6,32 @@ import "react-datepicker/dist/react-datepicker.css";
 export default function TableDatePicker() {
 
  const [date, setDate] = useState(new Date());
+ const [startDate, setStartDate] = useState(new Date());
+ const [endDate, setEndDate] = useState(new Date());
 
  return (
-   <DatePicker selected={date} onChange={date => setDate(date)} />
+    <div> 
+    <DatePicker
+        showTimeSelect
+        dateFormat="MMMM d, yyyy h:mmaa"
+        selected={startDate}
+        selectsStart
+        startDate={startDate}
+        endDate={endDate}
+        onChange={date => setStartDate(date)}
+/>
+
+    {/* Uncomment out if you want a end date and time */}
+    {/* <DatePicker
+        showTimeSelect
+        dateFormat="MMMM d, yyyy h:mmaa"
+        selected={startDate}
+        selectsEnd
+        startDate={startDate}
+        endDate={endDate}
+        minDate={startDate}
+        onChange={date => setEndDate(date)}
+/> */}
+   </div>
  );
 }
