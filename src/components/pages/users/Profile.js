3915b5bf74f56bc377d7d0f339b46889a29e10a0
elek8871 <elekmichelle@gmail.com>
2022-10-29 10:46:09 -0600

    adds user profile page

--- src/components/pages/users/Profile.js
+++ src/components/pages/users/Profile.js
@@ -1,8 +1,46 @@
+import { Link } from "react-router-dom"
+import { useState, UseEffect, useEffect } from "react"
+import axios from "axios"
+
+
 export default function Profile(){
+    const [pets, setPets] = useState([])
+    const [errorMessage, setErrorMessage] = useState([])
+
+    useEffect(()=>{
+        const getPets = async ()=>{
+            try{
+                const response = await axios.get(("http://localhost:8000/api/user/pet/", options))
+                setPets(response.data.pets)
+            }catch(err){
+                console.warn(err)
+                if(err.response){
+                    setErrorMessage(err.response.data.message)
+                }
+            }
+        }
+        getPets()
+    }, [])
+    console.log(pets)
+
+    const petList = pets.map(pet =>{
+        return(
+            // not sure if this is correct w django? I feel like the id is different
+            <div key= {pet._id}>
+                <Link to = {"/user/{pet._id}/pet/"}> {pet.name}'s diary</Link>
+            </div>
+        )
+    })
 
     return(
         <div>
-            <h1></h1>
+            <h1> Welome to Pet Pal </h1>
+            <p> Pet Pal is designed to keep you up to date on your pets appointments and needs</p>
+            {errorMessage}
+            {/* link to add a new pet */}
+            <Link to = "/pet/new"> Add your pet</Link>
+            {/* link to petdiary */}
+            <Link to="/pet/home"> Your pets diary </Link>
         </div>
     )
 }
